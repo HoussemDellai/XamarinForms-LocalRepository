abd2fbaf3b146576f0ffe587fe7c5ba7904e9a1a
Houssem Dellai <houssem.dellai@live.com>
2015-06-29 19:32:30 +0100

    Added Windows 8.1 project.

---
+++ XamarinForms.LocalRepository/XamarinForms.LocalRepository.Windows/SaveAndLoadTextWindows.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Xamarin.Forms;
+using XamarinForms.LocalRepository.Windows;
+
+[assembly: Dependency(typeof(SaveAndLoadTextWindows))]
+
+namespace XamarinForms.LocalRepository.Windows
+{
+    public class SaveAndLoadTextWindows : ISaveAndLoadText
+    {
+
+        private const string EmployeeFileName = "employee.txt";
+
+        public async Task<bool> SaveTextAsync(string text)
+        {
+            // Create sample file; replace if exists.
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            StorageFile sampleFile = await folder.CreateFileAsync(EmployeeFileName, CreationCollisionOption.ReplaceExisting);
+
+            try
+            {
+                await FileIO.WriteTextAsync(sampleFile, text);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<string> GetTextAsync()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile =
+                await storageFolder.GetFileAsync(EmployeeFileName);
+
+            try
+            {
+                return await FileIO.ReadTextAsync(sampleFile);
+            }
+            catch (Exception)
+            {
+                return String.Empty;
+            }
+        }
+    }
+}
